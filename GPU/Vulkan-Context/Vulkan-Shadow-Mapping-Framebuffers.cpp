7b210a0009806295fa269f024699802d31486750
BreadDev <wholewheatloafbread@outlook.com>
2024-05-21 01:40:10 +0800

    Finished Vulkan shadow mapping framebuffers

--- GPU/Vulkan-Context/Vulkan-Shadow-Mapping-Framebuffers.cpp
+++ GPU/Vulkan-Context/Vulkan-Shadow-Mapping-Framebuffers.cpp
@@ -5,19 +5,29 @@
 
 
 
+static GPULocalTexture g_textures[CASCADED_SHADOW_MAP_COUNT][MAX_LIGHT_COUNT] = { { VK_NULL_HANDLE } };
+
+
+
 void GPUFixedContext::build_shadowMappingFramebuffers(void) {
-	const VkExtent3D Extent = SHADOW_MAP_EXTENT;
+	VkExtent3D Extent = SHADOW_MAP_EXTENT;
+	uint32_t Divisor = 1;
+
 	for(uint32_t i = 0; i < CASCADED_SHADOW_MAP_COUNT; i++) {
+		Extent.width /= Divisor;
+		Extent.height /= Divisor;
 		for(uint32_t j = 0; j < MAX_LIGHT_COUNT; j++) {
-			build_localTexture(&m_shadowMappingAttachments[i][j], nullptr, VK_FORMAT_D32_SFLOAT, Extent, VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT);
+			build_localTexture(&g_textures[i][j], nullptr, VK_FORMAT_D32_SFLOAT, Extent, VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT);
 		}
+		Divisor++;
 	}
 	
+	Extent = SHADOW_MAP_EXTENT;
 	VkFramebufferCreateInfo CreateInfo = {
 		.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
 		.pNext = nullptr,
 		.flags = 0,
-		.renderPass = VK_NULL_HANDLE,
+		.renderPass = m_shadowMappingPass,
 		.attachmentCount = 1,
 		.pAttachments = nullptr,
 		.width = Extent.width,
@@ -25,12 +35,15 @@ void GPUFixedContext::build_shadowMappingFramebuffers(void) {
 		.layers = 1
 	};
 	
+	Divisor = 1;
 	for(uint32_t i = 0; i < CASCADED_SHADOW_MAP_COUNT; i++) {
-		CreateInfo.renderPass = m_shadowMappingPasses[i];
+		CreateInfo.width /= Divisor;
+		CreateInfo.height /= Divisor;
 		for(uint32_t j = 0; j < MAX_LIGHT_COUNT; j++) {
-			CreateInfo.pAttachments = &m_shadowMappingAttachments[i][j].view;
+			CreateInfo.pAttachments = &g_textures[i][j].view;
 			CHECK(vkCreateFramebuffer(m_logical, &CreateInfo, nullptr, &m_shadowMappingFramebuffers[i][j]))
 		}
+		Divisor++;
 	}
 }
 
@@ -38,7 +51,7 @@ void GPUFixedContext::ruin_shadowMappingFramebuffers(void) {
 	for(uint32_t i = 0; i < CASCADED_SHADOW_MAP_COUNT; i++) {
 		for(uint32_t j = 0; j < MAX_LIGHT_COUNT; j++) {
 			vkDestroyFramebuffer(m_logical, m_shadowMappingFramebuffers[i][j], nullptr);
-			ruin_localTexture(&m_shadowMappingAttachments[i][j]);
+			ruin_localTexture(&g_textures[i][j]);
 		}
 	}
 }

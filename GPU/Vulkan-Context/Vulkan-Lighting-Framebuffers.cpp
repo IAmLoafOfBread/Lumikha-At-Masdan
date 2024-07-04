f7ebf64d4e397dcff2affa5c0b11491705fac201
BreadDev <wholewheatloafbread@outlook.com>
2024-07-04 14:40:56 +0800

    Fixed lighting pipeline stuff (removed depth stencil attachment)

--- GPU/Vulkan-Context/Vulkan-Lighting-Framebuffers.cpp
+++ GPU/Vulkan-Context/Vulkan-Lighting-Framebuffers.cpp
@@ -5,42 +5,28 @@
 
 
 
-#define ATTACHMENT_COUNT 2
-
-static GPULocalTexture g_depthTexture = { VK_NULL_HANDLE };
-
-
-
 void GPUFixedContext::build_lightingFramebuffers(void) {
 	m_lightingFramebuffers = new VkFramebuffer[m_surfaceFrameCount];
 
-	build_localTexture(&g_depthTexture, nullptr, VK_FORMAT_D32_SFLOAT, m_surfaceExtent, VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT);
-
-	{
-		VkImageView Attachments[ATTACHMENT_COUNT] = { VK_NULL_HANDLE };
-		Attachments[1] = g_depthTexture.view;
-
-		const VkFramebufferCreateInfo CreateInfo = {
-			.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
-			.pNext = nullptr,
-			.flags = 0,
-			.renderPass = m_lightingPass,
-			.attachmentCount = ATTACHMENT_COUNT,
-			.pAttachments = Attachments,
-			.width = m_surfaceExtent.width,
-			.height = m_surfaceExtent.height,
-			.layers = 1
-		};
-		for (uint32_t i = 0; i < m_surfaceFrameCount; i++) {
-			Attachments[0] = m_presentViews[i];
-			CHECK(vkCreateFramebuffer(m_logical, &CreateInfo, nullptr, &m_lightingFramebuffers[i]))
-		}
+	VkFramebufferCreateInfo CreateInfo = {
+		.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
+		.pNext = nullptr,
+		.flags = 0,
+		.renderPass = m_lightingPass,
+		.attachmentCount = 1,
+		.pAttachments = nullptr,
+		.width = m_surfaceExtent.width,
+		.height = m_surfaceExtent.height,
+		.layers = 1
+	};
+	for (uint32_t i = 0; i < m_surfaceFrameCount; i++) {
+		CreateInfo.pAttachments = &m_presentViews[i];
+		CHECK(vkCreateFramebuffer(m_logical, &CreateInfo, nullptr, &m_lightingFramebuffers[i]))
 	}
 }
 
 void GPUFixedContext::ruin_lightingFramebuffers(void) {
 	for(uint32_t i = 0; i < m_surfaceFrameCount; i++) vkDestroyFramebuffer(m_logical, m_lightingFramebuffers[i], nullptr);
-	ruin_localTexture(&g_depthTexture);
 	delete[] m_lightingFramebuffers;
 }
 
--- GPU/Vulkan-Context/Vulkan-Lighting-Pass.cpp
+++ GPU/Vulkan-Context/Vulkan-Lighting-Pass.cpp
@@ -5,65 +5,49 @@
 
 
 
-#define ATTACHMENT_COUNT 2
-
-
-
 void GPUFixedContext::build_lightingPass(void) {
-	{
-		VkAttachmentDescription Descriptions[ATTACHMENT_COUNT] = { { 0 } };
-		{
-			const VkFormat Formats[ATTACHMENT_COUNT] = {m_surfaceFormat, VK_FORMAT_D32_SFLOAT};
-			for (uint32_t i = 0; i < ATTACHMENT_COUNT; i++) {
-				Descriptions[i].flags = 0;
-				Descriptions[i].format = Formats[i];
-				Descriptions[i].samples = VK_SAMPLE_COUNT_1_BIT;
-				Descriptions[i].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-				Descriptions[i].storeOp = Formats[i] != VK_FORMAT_D32_SFLOAT ? VK_ATTACHMENT_STORE_OP_STORE : VK_ATTACHMENT_STORE_OP_DONT_CARE;
-				Descriptions[i].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-				Descriptions[i].stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-				Descriptions[i].initialLayout = Formats[i] != VK_FORMAT_D32_SFLOAT ? VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL : VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-				Descriptions[i].finalLayout = Formats[i] != VK_FORMAT_D32_SFLOAT ? VK_IMAGE_LAYOUT_PRESENT_SRC_KHR : VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-			}
-		}
+	const VkAttachmentDescription Description = {
+		.flags = 0,
+		.format = m_surfaceFormat,
+		.samples = VK_SAMPLE_COUNT_1_BIT,
+		.loadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE,
+		.storeOp = VK_ATTACHMENT_STORE_OP_STORE,
+		.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE,
+		.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
+		.initialLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+		.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR
+	};
 
-		const VkAttachmentReference References[ATTACHMENT_COUNT] = {
-			{
-				.attachment = 0,
-				.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
-			},
-			{
-				.attachment = 1,
-				.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
-			},
-		};
+	const VkAttachmentReference Reference = {
+		.attachment = 0,
+		.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+	};
 
-		VkSubpassDescription Subpass = {
-			.flags = 0,
-			.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS,
-			.inputAttachmentCount = 0,
-			.pInputAttachments = nullptr,
-			.colorAttachmentCount = 1,
-			.pColorAttachments = References,
-			.pResolveAttachments = nullptr,
-			.pDepthStencilAttachment = &References[1],
-			.preserveAttachmentCount = 0,
-			.pPreserveAttachments = nullptr
-		};
+	const VkSubpassDescription Subpass = {
+		.flags = 0,
+		.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS,
+		.inputAttachmentCount = 0,
+		.pInputAttachments = nullptr,
+		.colorAttachmentCount = 1,
+		.pColorAttachments = &Reference,
+		.pResolveAttachments = nullptr,
+		.pDepthStencilAttachment = nullptr,
+		.preserveAttachmentCount = 0,
+		.pPreserveAttachments = nullptr
+	};
 
-		const VkRenderPassCreateInfo CreateInfo = {
-			.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO,
-			.pNext = nullptr,
-			.flags = 0,
-			.attachmentCount = ATTACHMENT_COUNT,
-			.pAttachments = Descriptions,
-			.subpassCount = 1,
-			.pSubpasses = &Subpass,
-			.dependencyCount = 0,
-			.pDependencies = nullptr
-		};
-		CHECK(vkCreateRenderPass(m_logical, &CreateInfo, nullptr, &m_lightingPass))
-	}
+	const VkRenderPassCreateInfo CreateInfo = {
+		.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO,
+		.pNext = nullptr,
+		.flags = 0,
+		.attachmentCount = 1,
+		.pAttachments = &Description,
+		.subpassCount = 1,
+		.pSubpasses = &Subpass,
+		.dependencyCount = 0,
+		.pDependencies = nullptr
+	};
+	CHECK(vkCreateRenderPass(m_logical, &CreateInfo, nullptr, &m_lightingPass))
 }
 
 void GPUFixedContext::ruin_lightingPass(void) {

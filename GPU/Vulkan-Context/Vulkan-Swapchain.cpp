58be84fdf1735c0ffa15b05b57533e1b2dd358e4
BreadDev <wholewheatloafbread@outlook.com>
2024-07-10 07:57:09 +0800

    Transitioned images now

--- GPU/Vulkan-Context/Vulkan-Swapchain.cpp
+++ GPU/Vulkan-Context/Vulkan-Swapchain.cpp
@@ -50,9 +50,50 @@ void GPUFixedContext::build_swapchain(void) {
 			.layerCount = 1
 		}
 	};
+
+	const VkSubmitInfo SubmitInfo = {
+		.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,
+		.pNext = nullptr,
+		.waitSemaphoreCount = 0,
+		.pWaitSemaphores = nullptr,
+		.pWaitDstStageMask = nullptr,
+		.commandBufferCount = 1,
+		.pCommandBuffers = &m_deferredRenderingCommandSet,
+		.signalSemaphoreCount = 0,
+		.pSignalSemaphores = nullptr
+	};
+
+	VkImageMemoryBarrier Barrier = {
+		.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+		.pNext = nullptr,
+		.srcAccessMask = 0,
+		.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,
+		.oldLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+		.newLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+		.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+		.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+		.image = VK_NULL_HANDLE,
+		.subresourceRange = {
+			.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+			.baseMipLevel = 0,
+			.levelCount = 1,
+			.baseArrayLayer = 0,
+			.layerCount = 1
+		},
+	};
+
+
+
 	for(uint32_t i = 0; i < m_surfaceFrameCount; i++) {
 		CreateInfo.image = Images[i];
 		CHECK(vkCreateImageView(m_logical, &CreateInfo, nullptr, &m_presentViews[i]))
+
+		Barrier.image = Images[i];
+		CHECK(vkBeginCommandBuffer(m_deferredRenderingCommandSet, &G_FIXED_COMMAND_BEGIN_INFO))
+		vkCmdPipelineBarrier(m_deferredRenderingCommandSet, VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, 0, 0, nullptr, 0, nullptr, 1, &Barrier);
+		vkEndCommandBuffer(m_deferredRenderingCommandSet);
+		CHECK(vkQueueSubmit(m_deferredRenderingCommandQueue, 1, &SubmitInfo, VK_NULL_HANDLE))
+		CHECK(vkQueueWaitIdle(m_deferredRenderingCommandQueue))
 	}
 	
 	delete[] Images;

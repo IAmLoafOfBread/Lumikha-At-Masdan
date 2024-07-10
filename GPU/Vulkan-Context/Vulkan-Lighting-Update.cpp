968eb33635b6dae03ba6fcdbe3b125263ca33e61
BreadDev <wholewheatloafbread@outlook.com>
2024-07-10 08:18:22 +0800

    All render images now have proper transitioning

--- GPU/Vulkan-Context/Vulkan-Lighting-Update.cpp
+++ GPU/Vulkan-Context/Vulkan-Lighting-Update.cpp
@@ -5,6 +5,9 @@
 
 
 
+static VkImageMemoryBarrier g_shadowBarriers[CASCADED_SHADOW_MAP_COUNT][MAX_LIGHT_COUNT] = { { { VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER } } };
+static VkImageMemoryBarrier g_geometryBarriers[GEOMETRY_PASS_COLOUR_ATTACHMENT_COUNT] = { { VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER } };
+
 static const VkClearValue g_clearValue = {0.0f, 0.0f, 0.0f, 1.0f};
 static VkRenderPassBeginInfo g_renderInfo = { VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO };
 static VkPipelineStageFlags g_waitStages[CASCADED_SHADOW_MAP_COUNT] = { 0 };
@@ -13,6 +16,42 @@ static VkSubmitInfo g_submitInfo = { VK_STRUCTURE_TYPE_SUBMIT_INFO };
 
 
 void GPUFixedContext::initialize_lightingUpdateData(void) {
+	for(uint32_t i = 0; i < CASCADED_SHADOW_MAP_COUNT; i++) {
+		for(uint32_t j = 0; j < MAX_LIGHT_COUNT; j++) {
+			g_shadowBarriers[i][j].sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+			g_shadowBarriers[i][j].pNext = nullptr;
+			g_shadowBarriers[i][j].srcAccessMask = VK_ACCESS_SHADER_READ_BIT;
+			g_shadowBarriers[i][j].dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+			g_shadowBarriers[i][j].oldLayout = VK_IMAGE_LAYOUT_READ_ONLY_OPTIMAL;
+			g_shadowBarriers[i][j].newLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+			g_shadowBarriers[i][j].srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+			g_shadowBarriers[i][j].dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+			g_shadowBarriers[i][j].image = m_shadowTextures[i][j].image;
+			g_shadowBarriers[i][j].subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			g_shadowBarriers[i][j].subresourceRange.baseMipLevel = 0;
+			g_shadowBarriers[i][j].subresourceRange.levelCount = 1;
+			g_shadowBarriers[i][j].subresourceRange.baseArrayLayer = 0;
+			g_shadowBarriers[i][j].subresourceRange.layerCount = 1;
+		}
+	}
+
+	for (uint32_t i = 0; i < GEOMETRY_PASS_COLOUR_ATTACHMENT_COUNT; i++) {
+		g_geometryBarriers[i].sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+		g_geometryBarriers[i].pNext = nullptr;
+		g_geometryBarriers[i].srcAccessMask = VK_ACCESS_SHADER_READ_BIT;
+		g_geometryBarriers[i].dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+		g_geometryBarriers[i].oldLayout = VK_IMAGE_LAYOUT_READ_ONLY_OPTIMAL;
+		g_geometryBarriers[i].newLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+		g_geometryBarriers[i].srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+		g_geometryBarriers[i].dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+		g_geometryBarriers[i].image = m_geometryTextures[i].image;
+		g_geometryBarriers[i].subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+		g_geometryBarriers[i].subresourceRange.baseMipLevel = 0;
+		g_geometryBarriers[i].subresourceRange.levelCount = 1;
+		g_geometryBarriers[i].subresourceRange.baseArrayLayer = 0;
+		g_geometryBarriers[i].subresourceRange.layerCount = 1;
+	}
+
 	g_renderInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
 	g_renderInfo.pNext = nullptr;
 	g_renderInfo.renderPass = m_lightingPass;
@@ -49,6 +88,11 @@ void GPUFixedContext::draw_lightingUpdate(void) {
 	vkCmdBeginRenderPass(m_deferredRenderingCommandSet, &g_renderInfo, VK_SUBPASS_CONTENTS_INLINE);
 	vkCmdDraw(m_deferredRenderingCommandSet, 4, 1, 0, 0);
 	vkCmdEndRenderPass(m_deferredRenderingCommandSet);
+	
+	for(uint32_t i = 0; i < CASCADED_SHADOW_MAP_COUNT; i++) {
+		vkCmdPipelineBarrier(m_deferredRenderingCommandSet, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT, 0, 0, nullptr, 0, nullptr, m_lightCount, g_shadowBarriers[i]);
+	}
+	vkCmdPipelineBarrier(m_deferredRenderingCommandSet, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, 0, 0, nullptr, 0, nullptr, GEOMETRY_PASS_COLOUR_ATTACHMENT_COUNT, g_geometryBarriers);
 	
 	vkEndCommandBuffer(m_deferredRenderingCommandSet);
 	vkQueueSubmit(m_deferredRenderingCommandQueue, 1, &g_submitInfo, VK_NULL_HANDLE);
--- GPU/Vulkan-Context/Vulkan-Present-Update.cpp
+++ GPU/Vulkan-Context/Vulkan-Present-Update.cpp
@@ -5,11 +5,30 @@
 
 
 
+static VkImageMemoryBarrier g_barrier = { VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER };
+
 static VkPresentInfoKHR g_presentInfo = { VK_STRUCTURE_TYPE_PRESENT_INFO_KHR };
+
+static VkSubmitInfo g_submitInfo = { VK_STRUCTURE_TYPE_SUBMIT_INFO };
 
 
 
 void GPUFixedContext::initialize_presentUpdateData(void) {
+	g_barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+	g_barrier.pNext = nullptr;
+	g_barrier.srcAccessMask = VK_ACCESS_MEMORY_READ_BIT;
+	g_barrier.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+	g_barrier.oldLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+	g_barrier.newLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+	g_barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+	g_barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+	g_barrier.image = VK_NULL_HANDLE;
+	g_barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+	g_barrier.subresourceRange.baseMipLevel = 0;
+	g_barrier.subresourceRange.levelCount = 1;
+	g_barrier.subresourceRange.baseArrayLayer = 0;
+	g_barrier.subresourceRange.layerCount = 1;
+
 	g_presentInfo.pNext = nullptr;
 	g_presentInfo.waitSemaphoreCount = 1;
 	g_presentInfo.pWaitSemaphores = &m_renderFinishedSemaphore;
@@ -17,10 +36,27 @@ void GPUFixedContext::initialize_presentUpdateData(void) {
 	g_presentInfo.pSwapchains = &m_swapchain;
 	g_presentInfo.pImageIndices = &m_currentImageIndex;
 	g_presentInfo.pResults = nullptr;
+
+	g_submitInfo.pNext = nullptr;
+	g_submitInfo.waitSemaphoreCount = 0;
+	g_submitInfo.pWaitSemaphores = nullptr;
+	g_submitInfo.pWaitDstStageMask = nullptr;
+	g_submitInfo.commandBufferCount = 1;
+	g_submitInfo.pCommandBuffers = &m_deferredRenderingCommandSet;
+	g_submitInfo.signalSemaphoreCount = 0;
+	g_submitInfo.pSignalSemaphores = nullptr;
 }
 
 void GPUFixedContext::submit_presentUpdate(void) {
+	g_barrier.image = m_presentImages[m_currentImageIndex];
 	vkQueuePresentKHR(m_deferredRenderingCommandQueue, &g_presentInfo);
+	CHECK(vkQueueWaitIdle(m_deferredRenderingCommandQueue))
+
+	CHECK(vkBeginCommandBuffer(m_deferredRenderingCommandSet, &G_FIXED_COMMAND_BEGIN_INFO))
+	vkCmdPipelineBarrier(m_deferredRenderingCommandSet, VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, 0, 0, nullptr, 0, nullptr, 1, &g_barrier);
+	vkEndCommandBuffer(m_deferredRenderingCommandSet);
+	CHECK(vkQueueSubmit(m_deferredRenderingCommandQueue, 1, &g_submitInfo, VK_NULL_HANDLE))
+	CHECK(vkQueueWaitIdle(m_deferredRenderingCommandQueue))
 }
 
 
